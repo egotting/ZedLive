56b00082765891932df308e9dadda8b32cff4311
egotting <henricodss@gmail.com>
2026-01-20 20:06:12 -0300

    feature: implement value object for password

--- src/domain/ZedLive.Domain/ValueObjects/PasswordObject.cs
+++ src/domain/ZedLive.Domain/ValueObjects/PasswordObject.cs
@@ -1,6 +1,49 @@
-﻿namespace ZedLive.Domain.ValueObjects;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
 
-public class PasswordObject
+namespace ZedLive.Domain.ValueObjects;
+
+public abstract class PasswordObject : ValueObjects
 {
-    
+    private const int SaltSize = 16;
+    private const int HashSize = 32;
+    private const int Interations = 100000;
+
+    private static readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA512;
+
+    [NotNull] private string Value { get; set; } = string.Empty;
+
+    protected PasswordObject(string value)
+    {
+        if (string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value))
+            throw new ArgumentException("Password Invalid");
+        Value = value.Length switch
+        {
+            < 1 => throw new ArgumentException("Need until 8 characters"),
+            > 8 => throw new ArgumentException("Cannot pass the 8 characters"),
+            _ => value
+        };
+    }
+
+    public static string Hash(string value)
+    {
+        if (string.IsNullOrEmpty(value)) throw new ArgumentException("Need put a value");
+        byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(value, salt, Interations, Algorithm, HashSize);
+        return $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
+    }
+
+    public static bool Verify(string value, string valueHashed)
+    {
+        if (string.IsNullOrEmpty(value) || string.IsNullOrEmpty(valueHashed))
+            throw new ArgumentException("Need put a value");
+        string[] parts = valueHashed.Split("-");
+
+        byte[] hash = Convert.FromHexString(parts[0]);
+        byte[] salt = Convert.FromHexString(parts[1]);
+
+        byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(value, salt, Interations, Algorithm, HashSize);
+
+        return CryptographicOperations.FixedTimeEquals(hash, inputHash);
+    }
 }
---
+++ src/tests/ZedLive.Tests/ValueObjects/PasswordObject.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ZedLive.Tests.ValueObjects;
+
+public class PasswordObject
+{
+    // TESTE HASHPASSOWORD
+    [Fact]
+    public void HashPasswordTest()
+    {
+        const int SaltSize = 16;
+        const int HashSize = 32;
+        const int Interations = 100000;
+        const string password = "password123";
+        HashAlgorithmName Algorith = HashAlgorithmName.SHA512;
+
+        
+        // create
+        byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+        byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Interations, Algorith, HashSize);
+        
+        // verify
+        string passwordHash = $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
+        string[] parts = passwordHash.Split("-");
+        byte[] hashVerify = Convert.FromHexString(parts[0]);
+        byte[] saltVerify = Convert.FromHexString(parts[1]);
+
+        byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, saltVerify, Interations, Algorith, HashSize);
+
+        var verify = CryptographicOperations.FixedTimeEquals(hashVerify, inputHash);
+        Assert.True(verify);
+    }
+}
