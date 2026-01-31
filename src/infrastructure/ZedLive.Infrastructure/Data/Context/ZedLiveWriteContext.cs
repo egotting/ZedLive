4469cee1c065641626b3d57a7bd9cf6b7b61b11d
egotting <henricodss@gmail.com>
2026-01-31 19:05:05 -0300

    remove for now

--- src/infrastructure/ZedLive.Infrastructure/Data/Context/ZedLiveWriteContext.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ZedLive.Infrastructure.Data.Context;
-
-public sealed class ZedLiveWriteContext
-{
-
-
-    private static bool IsWrite(Type type) => type.FullName?.Contains("Mapper.Write") ?? false;
-}
