0ffdd571183ab61e87b7db02ce9d7c6c3f4557ab
egotting <henricodss@gmail.com>
2026-01-31 19:11:38 -0300

    feat: implementação geração do token acesso

---
+++ src/application/ZedLive.Application/Services/Jwt/Jwt.cs
@@ -0,0 +1,95 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
+using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using ZedLive.Application.Services.Stream;
+using ZedLive.Domain.Contracts.Users.Configuration;
+using ZedLive.Domain.User;
+using ZedLive.Domain.ValueObjects.StructType;
+
+namespace ZedLive.Application.Services.Jwt;
+
+public sealed class Jwt(JwtOptions _jwtOptions, ILogger<Jwt> _logger) : IJwt
+{
+    public string Generate(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_jwtOptions.SecretKey);
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.NameIdentifier, user.Id.ToString()),
+            new(ClaimTypes.Name, user.Login),
+            new(ClaimTypes.Email, user.Email.ToString()),
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Jti, new DateTimeOffset(DateTime.UtcNow).ToUnixTimeSeconds().ToString(),
+                ClaimValueTypes.Integer32)
+        };
+
+        var tokenDecriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.AddMinutes(_jwtOptions.AccessTokenExpirationMinutes),
+            Issuer = _jwtOptions.Issuer,
+            Audience = _jwtOptions.Audience,
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(key),
+                SecurityAlgorithms.HmacSha256Signature
+            )
+        };
+        var token = tokenHandler.CreateToken(tokenDecriptor);
+        var tokenConvert = tokenHandler.WriteToken(token);
+        _logger.LogInformation("Stream Key generated for user {Username}", user.Email);
+        return tokenConvert;
+    }
+
+    public string GenerateRefreshToken()
+    {
+        var randomNumber = new byte[64];
+        using var rng = RandomNumberGenerator.Create();
+        rng.GetBytes(randomNumber);
+        return Convert.ToBase64String(randomNumber);
+    }
+
+    public ClaimsPrincipal? ValidateToken(string token, bool validateLifetime = true)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(_jwtOptions.SecretKey);
+
+        try
+        {
+            var validateParameters = new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = true,
+                ValidIssuer = _jwtOptions.Issuer,
+                ValidateAudience = true,
+                ValidAudience = _jwtOptions.Audience,
+                ValidateLifetime = validateLifetime,
+                ClockSkew = TimeSpan.FromMinutes(1),
+            };
+            var principal = tokenHandler.ValidateToken(token, validateParameters, out var validatedToken);
+
+            if (validatedToken is JwtSecurityToken jwtToken &&
+                !jwtToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+            {
+                _logger.LogWarning("Token validated failed: invalid algorithm {Algorithm}", jwtToken.Header.Alg);
+                return null;
+            }
+
+            return principal;
+        }
+        catch (SecurityTokenException e)
+        {
+            _logger.LogWarning(e, "Token validated failed");
+            return null;
+        }
+    }
+
+    public ClaimsPrincipal? GetPrincipalFromExpiredToken(string token)
+    {
+        return ValidateToken(token, false);
+    }
+}
--- src/domain/ZedLive.Domain/ValueObjects/StructType/JwtOptions.cs
+++ src/domain/ZedLive.Domain/ValueObjects/StructType/JwtOptions.cs
@@ -2,10 +2,9 @@
 
 public record JwtOptions
 {
-    // WARNING: THIS FILE CANNOT BE SHOWING TO ANYONE
-    public string SecretKey { get; set; } = "</zM+u:oCSiGgw,l@$[}n£NQ837h=#,dO:AtegMA&%";
+    public string SecretKey { get; set; } = string.Empty;
     public string Issuer { get; set; } = string.Empty;
     public string Audience { get; set; } = string.Empty;
-    public int AccessTokenExpirationMinutes { get; set; } = 15;
-    public int RefreshTokenExpirationDays { get; set; } = 7;
+    public int AccessTokenExpirationMinutes { get; set; }
+    public int RefreshTokenExpirationDays { get; set; }
 }
