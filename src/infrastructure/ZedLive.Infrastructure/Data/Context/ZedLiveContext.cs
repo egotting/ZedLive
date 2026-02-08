2b74eb599f3ec3c68201e5a3944fddbedd73274b
egotting <henricodss@gmail.com>
2026-02-08 02:43:07 -0300

    no updates

--- src/infrastructure/ZedLive.Infrastructure/Data/Context/ZedLiveContext.cs
+++ src/infrastructure/ZedLive.Infrastructure/Data/Context/ZedLiveContext.cs
@@ -5,8 +5,8 @@ namespace ZedLive.Infrastructure.Data.Context;
 
 public sealed class ZedLiveContext(DbContextOptions opt) : DbContext(opt)
 {
-
     public DbSet<User> Users { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfigurationsFromAssembly(typeof(ZedLiveContext).Assembly);
