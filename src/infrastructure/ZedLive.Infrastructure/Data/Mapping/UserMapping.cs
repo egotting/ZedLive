da25091e43aefa4bde044981db939f0205dfa6f3
egotting <henricodss@gmail.com>
2026-02-12 17:14:19 -0300

    feat: configurate streamkey in user mapping

---
+++ src/infrastructure/ZedLive.Infrastructure/Data/Mapping/UserMapping.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ZedLive.Domain.User;
+
+namespace ZedLive.Infrastructure.Data.Mapping;
+
+public class UserMapping : IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("tb_usuarios", "dbo")
+            .HasKey(x => x.Id);
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Login)
+            .HasMaxLength(100)
+            .IsUnicode(false);
+        builder.HasIndex(x => x.Login)
+            .IsUnique();
+        builder.OwnsOne(e => e.Email, email =>
+        {
+            email.Property(e => e.Value)
+                .HasColumnName("email")
+                .HasColumnType("varchar")
+                .HasMaxLength(266);
+            email.HasIndex(x => x.Value)
+                .IsUnique();
+        });
+
+        builder.OwnsOne(x => x.Password, pass =>
+        {
+            pass.Property(p => p.Value)
+                .HasColumnName("password")
+                .HasColumnType("varchar")
+                .HasMaxLength(8)
+                .IsRequired();
+        });
+
+        builder.Property(x => x.StreamKey)
+            .HasColumnName("stream_key")
+            .HasMaxLength(250);
+        builder.HasIndex(x => x.StreamKey)
+            .IsUnique();
+        
+        builder.OwnsMany<StatusUser>("Status", b =>
+        {
+            b.WithOwner().HasForeignKey("Id");
+            b.ToTable("tb_status_user", "dbo");
+            b.Property<long>("Id");
+            b.Property<string>("Value").HasColumnType("varchar");
+            b.HasKey("Id", "Value");
+        });
+    }
+}
