5e4bfa48edd4ca4cbc0d0692c5084d5412a59db3
egotting <henricodss@gmail.com>
2026-03-12 20:38:15 -0300

    Feat: Configure Scalar and ModalState validate

--- src/api/ZedLive.Api/Startup.cs
+++ src/api/ZedLive.Api/Startup.cs
@@ -1,6 +1,11 @@
 ﻿using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi;
+using Npgsql.Internal.Postgres;
+using Scalar.AspNetCore;
+using ZedLive.Domain.ValueObjects;
 using ZedLive.Domain.ValueObjects.StructType;
 using ZedLive.IoC;
 
@@ -21,22 +26,81 @@ internal sealed class Startup
     {
         services.AddControllers();
 
+        #region Scalar
+
+        services.AddOpenApi(opt =>
+        {
+            opt.AddDocumentTransformer((document, context, cancellationToken) =>
+            {
+                document.Components ??= new OpenApiComponents();
+                document.Components.SecuritySchemes ??=
+                    new Dictionary<string, IOpenApiSecurityScheme>();
+                document.Components.SecuritySchemes["Bearer"] = new OpenApiSecurityScheme
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header
+                };
+
+                document.Security ??= new List<OpenApiSecurityRequirement>();
+
+                document.Security.Add(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecuritySchemeReference("Bearer"),
+                        new List<string>(Array.Empty<string>())
+                    }
+                });
+
+                return Task.CompletedTask;
+            });
+        });
+
+        #endregion
+
+        #region ModelState
+
+        services.Configure<ApiBehaviorOptions>(opt =>
+        {
+            opt.InvalidModelStateResponseFactory = context =>
+            {
+                var errors = context.ModelState
+                    .Where(x => x.Value.Errors.Count > 0)
+                    .Select(x => new
+                    {
+                        Field = x.Key,
+                        Error = x.Value?.Errors.First().ErrorMessage
+                    });
+                return new BadRequestObjectResult(new
+                {
+                    Message = "Validation erros",
+                    Error = errors
+                });
+            };
+        });
+
+        #endregion
+
         #region Options
+
         services.AddOptions<StreamOptions>()
             .BindConfiguration("Stream");
         services.AddOptions<JwtOptions>()
             .BindConfiguration("Jwt");
         services.AddOptions<ConnectionStringsOptions>()
             .BindConfiguration("DbConnection");
+
         #endregion
 
         #region Dependencies
+
         services.DependencyInjection(_configuration);
+
         #endregion Dependencies
 
         #region Configuration Authentication & Authorization
-        services.AddOptions<JwtOptions>()
-            .BindConfiguration("Jwt");
 
         services.AddAuthentication(opt =>
             {
@@ -75,14 +139,29 @@ internal sealed class Startup
                 };
             });
         services.AddAuthorization();
+
         #endregion Configuration Authentication & Authorization
     }
 
     public void Configure(IApplicationBuilder app)
     {
         app.UseCors(builder => { builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin(); });
+
         app.UseHttpsRedirection();
         app.UseRouting();
-        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.UseEndpoints(ep =>
+        {
+            ep.MapControllers();
+            ep.MapOpenApi();
+            ep.MapScalarApiReference(opt =>
+            {
+                opt.WithTitle("ZedLive")
+                    .WithTheme(ScalarTheme.Purple);
+            });
+        });
     }
 }
