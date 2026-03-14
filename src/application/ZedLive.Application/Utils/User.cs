805e9577f49fae8bec4e4a08fd576276de241476
egotting <henricodss@gmail.com>
2026-03-14 00:21:02 -0300

    feat: util to get id user jwt

---
+++ src/application/ZedLive.Application/Utils/User.cs
@@ -0,0 +1,14 @@
+﻿using System.Security.Claims;
+
+namespace ZedLive.Application.Utils;
+
+internal static class User
+{
+    public static int GetUserId(this ClaimsPrincipal claims)
+    {
+        int id;
+        var claimUser = claims.FindFirst("uid");
+        int.TryParse(claimUser?.Value, out id);
+        return id;
+    }
+}
